4d0b52a0a1b2d5b2afae67c39afdf0f03c1003a2
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2010-04-26 22:07:19 +0530

    Take into consideration when the app is not behind a reverse proxy

--- recaptcha_comments/views.py
+++ recaptcha_comments/views.py
@@ -4,7 +4,7 @@ def validate_and_submit(request, next=None):
     # The next three lines are slightly hackish but there's not
     # much that can be done if we have to pass the ip
     # address to recaptcha
-    ip_address = request.META.get('HTTP_X_FORWARDED_FOR', None)
+    ip_address = request.META.get('HTTP_X_FORWARDED_FOR', request.META.get('REMOTE_ADDR', None))
     post_querydict = request.POST.copy()
     post_querydict.update({'remoteip': ip_address})
     request.POST = post_querydict
